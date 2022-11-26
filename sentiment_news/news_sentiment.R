02b0ff1823f0b1f120b8349c1e6224a7fa25bd77
syrchanan <cdawg004@gmail.com>
2022-11-26 12:51:08 -0800

    delete old

--- sentiment_news/news_sentiment.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-
-url <- "http://data.gdeltproject.org/gdeltv3/iatv/ngrams/FILELIST-CNN.TXT"
-
-R.utils::gunzip("20220520.CNN.1gram.txt.gz")
-
-ngrams <- read_tsv("20220520.CNN.1gram.txt", col_names = c("date", "network", "time", "word", "count"))
